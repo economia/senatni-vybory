2ee0252d79b1859fc713e4cffb370e942241e46e
Marcel Šulek <marcel@sulek.eu>
2013-08-30 11:29:45 +0200

    generator X-ove skaly

--- www/js/Barchart.js
+++ www/js/Barchart.js
@@ -1,5 +1,5 @@
 (function(){
-  var Dimensionable, Barchart;
+  var Dimensionable, XScale, Barchart;
   Dimensionable = {
     margin: {
       top: 0,
@@ -14,22 +14,41 @@
       return this.height = this.fullWidth - this.margin.top - this.margin.bottom;
     }
   };
+  XScale = {
+    recomputeXScale: function(){
+      var x$, ref$;
+      x$ = (ref$ = this.x) != null
+        ? ref$
+        : this.x = d3.scale.ordinal();
+      x$.rangeRoundBands([0, this.width], 0.1);
+      x$.domain(this.data.map(function(it){
+        return it.year;
+      }));
+      return x$;
+    }
+  };
   window.Barchart = Barchart = (function(){
     Barchart.displayName = 'Barchart';
     var prototype = Barchart.prototype, constructor = Barchart;
     importAll$(prototype, arguments[0]);
+    importAll$(prototype, arguments[1]);
     function Barchart(parentSelector, data){
-      var x$;
+      var x$, y$;
       this.parentSelector = parentSelector;
       this.data = data;
       this.computeDimensions(650, 600);
+      this.recomputeXScale();
       x$ = this.svg = d3.selectAll(this.parentSelector).append('svg');
       x$.attr('class', 'barchart');
       x$.attr('width', this.fullWidth);
       x$.attr('height', this.fullHeight);
+      y$ = this.content = this.svg.append('g');
+      y$.attr('class', 'content');
+      y$.attr('transform', "translate(" + this.margin.left + ", " + this.margin.top + ")");
+      this.recomputeXScale();
     }
     return Barchart;
-  }(Dimensionable));
+  }(Dimensionable, XScale));
   function importAll$(obj, src){
     for (var key in src) obj[key] = src[key];
     return obj;
--- www/ls/Barchart.ls
+++ www/ls/Barchart.ls
@@ -8,11 +8,26 @@ Dimensionable =
         @width = @fullWidth - @margin.left - @margin.right
         @height = @fullWidth - @margin.top - @margin.bottom
 
+XScale =
+    recomputeXScale: ->
+        @x ?= d3.scale.ordinal!
+            ..rangeRoundBands [0 @width], 0.1
+            ..domain @data.map (.year)
 
-window.Barchart = class Barchart implements Dimensionable
+
+window.Barchart = class Barchart implements Dimensionable, XScale
     (@parentSelector, @data) ->
         @computeDimensions 650 600
+        @recomputeXScale!
         @svg = d3.selectAll @parentSelector .append \svg
             ..attr \class \barchart
             ..attr \width @fullWidth
             ..attr \height @fullHeight
+        @content = @svg.append \g
+            ..attr \class \content
+            ..attr \transform "translate(#{@margin.left}, #{@margin.top})"
+
+
+        @recomputeXScale!
+
+
